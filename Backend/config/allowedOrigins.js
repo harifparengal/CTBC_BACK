66d7d0b0cca3a50dbd2948a131efc090a8925842
harifparengal <71120217+harifparengal@users.noreply.github.com>
2023-12-27 20:05:43 +0530

    Update allowedOrigins.js

--- Backend/config/allowedOrigins.js
+++ Backend/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:5173","http://localhost:5174"];
+const allowedOrigins = ["http://localhost:5173","http://localhost:5174","https://ctbci.org","http://ctbci.org"];
 
 module.exports = allowedOrigins;
